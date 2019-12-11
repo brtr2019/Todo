5045de7d8d7338c3e9b7c0a91fd3c40bca0bd840
brtr2019 <bradulovigor1983@gmail.com>
2019-12-11 13:43:13 +0000

    Create component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
+const TodoList = () => {
+	return (
+		<ul>
+
+			<li>Learn React</li>
+			<li>Build Awesome App</li>
+		</ul>
+	)
+}
+
+const AppHeader = () => {
+	return <h1> My Todo List</h1>
+}
+
+const SearchPanel = () => {
+	return <input type="text" placeholder="Search" />
+}
+
+const App = () => {
+	return (
+		<div>
+			<AppHeader />
+			<SearchPanel />
+			<TodoList />
+		</div>
+	)
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
