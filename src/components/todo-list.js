6833da01c5cab5379a6d77deb7dc962d9ebf9296
brtr2019 <bradulovigor1983@gmail.com>
2019-12-17 14:55:30 +0000

    другая реализация массива

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,10 +2,19 @@ import React from 'react';
 import {TodoListItem} from './todo-list-item';
 
 export const TodoList = ({todos}) => {
+
+	const firstEl = (
+		<li><TodoListItem label={todos[0].label} important={todos[0].important} /></li>
+	)
+
+	const secEl = (
+		<li><TodoListItem label={todos[1].label} important={todos[1].important}/></li> 
+	)
+
 	return (
 		<ul>
-			<li><TodoListItem label={todos[0].label} important={todos[0].important} /></li>
-			<li><TodoListItem label={todos[1].label} important={todos[1].important}/></li> 
+			{firstEl}
+			{secEl}
 		</ul>
 	)
 }
