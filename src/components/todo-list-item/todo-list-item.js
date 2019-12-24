ebc0ca52ecbc48bee6c4cd8f2726b910ae46d90e
brtr2019 <bradulovigor1983@gmail.com>
2019-12-24 13:29:22 +0000

    Use Events for TodoListItem

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,20 +3,29 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-	render(){
 
+	onLabelClick=()=>{
+		//console.log(`Done: ${this.props.label}`);
+		alert(`Done: ${this.props.label}`);
+	}
+		
+	render(){
+	
 	const { label, important = false } = this.props;	
-  
-	const style = {
+  const style = {
     color: important ? 'steelblue' : 'black',
     fontWeight: important ? 'bold' : 'normal'
   };
+
+	
+
+	
 
   return (
     <span className="todo-list-item">
       <span
         className="todo-list-item-label"
-        style={style}>
+        style={style} onClick={this.onLabelClick}>
         {label}
       </span>
 
